41f8bae69962bb605b3e5d92d17bd08876e04c07
kkitaaa <102261574+kkitaaa@users.noreply.github.com>
2026-08-25 18:59:54 -0400

    Implementar API Gateway

--- services/api-gateway/main.go
+++ services/api-gateway/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"os"
+
 	"github.com/gin-gonic/gin"
 	"github.com/meloop/api-gateway/routes"
 	"github.com/meloop/services/common/httpresponse"
@@ -8,15 +10,20 @@ import (
 )
 
 func main() {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+
 	logger := logging.New("api-gateway")
-	logger.Info("service_started", "port", 8080)
+	logger.Info("service_started", "port", port)
 	router := gin.New()
 	router.Use(logging.GinMiddleware(logger))
 	router.Use(httpresponse.GinRecoveryWithLogger(logger))
 
 	routes.SetupRoutes(router)
 
-	if err := router.Run(":8080"); err != nil {
+	if err := router.Run(":" + port); err != nil {
 		logger.Error("service_stopped", "error", err)
 	}
 }
