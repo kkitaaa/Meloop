5c59a0d7d81915bf72d6d8c40e7cc8c08fd36768
Alan Cofré <acofre2019@alu.uct.cl>
2026-09-14 21:12:47 -0300

    fix: modificar expectativa de widget_test
    Flutter test de CI/Github Actions no validaba correctamente por lo que se tuvo que hacer esa modificacion.

--- app/flutter_app/test/widget_test.dart
+++ app/flutter_app/test/widget_test.dart
@@ -4,6 +4,6 @@ import 'package:flutter_test/flutter_test.dart';
 void main() {
   testWidgets('Initial structure smoke test', (WidgetTester tester) async {
     await tester.pumpWidget(const MeloopApp());
-    expect(find.text('Meloop'), findsOneWidget);
+    expect(find.text('Iniciar Sesión'), findsOneWidget);
   });
 }
